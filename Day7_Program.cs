d3649a9f2d328d41ab95816b96da02511247760c
bhargavvaghela <45867652+bhargavvaghela@users.noreply.github.com>
2020-12-10 16:39:21 +0530

    Update Day7_Program.cs

--- Day7_Program.cs
+++ Day7_Program.cs
@@ -11,46 +11,21 @@ namespace Day7Assignment
         static void Main()
         {
             Employee1 e1 = new Employee1(E1_InvalidEmpName, E1_InvalidEmpBasic, E1_InvalidEmpDepNo,  "amol", 12000, 1);
-            //e1.NAME = "amol";
-            ////string E1name = e1.NAME;
-            //e1.BASIC = 12000;
-            ////decimal E1Basic = e1.BASIC;
-            //e1.DEPTNO = 1;
-            //short E1DepNo = e1.DEPTNO;
-            //e1 = new Employee1(E1name, E1Basic, E1DepNo);
+           
 
             Console.WriteLine("Empid: " + e1.EMPNO + ", Name: " + e1.NAME + ", Basic salary: " + e1.BASIC + ", Dept No: " + e1.DEPTNO + ", Net salary: " + e1.GetNetSalary());
             Console.ReadLine();
 
             Employee1 e2 = new Employee1(E2_InvalidEmpName, E2_InvalidEmpBasic, E2_InvalidEmpDepNo, "amol", 2000, 1);
-            //e2.InvalidEmpName += E2_InvalidEmpName;
-            //e2.InvalidEmpDepNo += E2_InvalidEmpDepNo;
-            //e2.InvalidEmpBasic += E2_InvalidEmpBasic;
+           
 
-            //e2.NAME = "amol";
-            //string E2name = e2.NAME;
-            //e2.BASIC = 2000;
-            //decimal E2Basic = e2.BASIC;
-            //e2.DEPTNO = 1;
-            //short E2DepNo = e2.DEPTNO;
-           // e2 = new Employee1("amol", 2000, 1);
-
-            //e2 = new Employee1("Amol", 25000);
+           
             Console.WriteLine("Empid: " + e2.EMPNO + ", Name: " + e2.NAME + ", Basic salary: " + e2.BASIC + ", Net salary: " + e2.GetNetSalary());
             Console.ReadLine();
 
             Employee1 e3 = new Employee1(E3_InvalidEmpName, E3_InvalidEmpBasic, E3_InvalidEmpDepNo, "", 2000);
 
-            //e3.InvalidEmpName += E3_InvalidEmpName;
-            //e3.InvalidEmpDepNo += E3_InvalidEmpDepNo;
-            //e3.InvalidEmpBasic += E3_InvalidEmpBasic;
-            //e3.NAME = "";
-            //string E3name = e3.NAME;
-            //e3.BASIC = 60;
-            //decimal E3Basic = e3.BASIC;
-            //e3.DEPTNO = 1;
-            //short E3DepNo = e3.DEPTNO;
-          //  e3 = new Employee1(E3name, E3Basic);
+           
             Console.WriteLine("Empid: " + e3.EMPNO + ", Name: " + e3.NAME + ", Basic salary: " + e3.BASIC + ", Net salary: " + e3.GetNetSalary());
             Console.ReadLine();
 
@@ -153,7 +128,6 @@ namespace Day7Assignment
         private decimal Basic;
         private short DeptNo;
 
-        //public static int counter;
 
         public event InvalidEmpNameHandlder InvalidEmpName;
         public event InvalidEmpBasicHandlder InvalidEmpBasic;
@@ -162,8 +136,7 @@ namespace Day7Assignment
         #region Constructors
         public Employee1()
         {
-            //EmpNo++;
-            //this.EMPNO += EmpNo;
+          
         }
         public Employee1(InvalidEmpNameHandlder InvalidEmpName, InvalidEmpBasicHandlder InvalidEmpBasic, InvalidEmpDepNoHandlder InvalidEmpDepNo,string Name, decimal Basic, short DeptNo)
         {
